f54099261992b64305da8f084b0989903a80c39a
12xy12xy <content.xexy@gmail.com>
2024-10-22 13:29:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(process.env.PORT || 8080, () => console.log('Server listening'));
